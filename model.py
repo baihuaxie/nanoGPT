072b2fe836cc9ea7c7b4260d7ead5681bb126a61
baihuaxie <xie_baihua@163.com>
2023-02-15 07:25:46 +0800

    Update model.py

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ https://github.com/openai/gpt-2/blob/master/src/model.py
 https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
 """
 
+
 import math
 import inspect
 from dataclasses import dataclass
